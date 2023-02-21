05d02f5a25222145935bb82772706217616209f2
Phat <nguyenphatit.nl@gmail.com>
2023-02-22 00:02:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+  <main>
+    <router-view />
+  </main>
+</template>
+
